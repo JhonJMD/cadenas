e724df39ee31e1a4b358360b66c9a0f4902eab0d
Jhon Jario <152086681+JhonJMD@users.noreply.github.com>
2023-12-14 14:47:52 -0500

    Cadenas Python

---
+++ myCadena.py
@@ -0,0 +1,13 @@
+mensaje = "Bienvenidos a python"
+valor = "4"
+#Metodos
+print(f"{mensaje.capitalize()}")
+print(f"{mensaje.upper()}")
+print(f"{mensaje.lower()}")
+print(f"{mensaje.swapcase()}")
+print(f"{mensaje.count('a')}")
+print(f"{mensaje.count('n')}")
+print(f"{len(mensaje)}")
+print(f"{valor.isalnum()}")
+print(f"{mensaje.isalpha()}")
+print(f"{valor.zfill(4)}")
